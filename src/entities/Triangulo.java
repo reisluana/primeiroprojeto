7755a9383a5968d18113940cc7efb97bed140b9e
Luana Reis <reisluanag@gmail.com>
2023-05-29 00:28:10 -0300

    classes, objetos e atributos

---
+++ src/entities/Triangulo.java
@@ -0,0 +1,7 @@
+package entities;
+//estamos considerando que o triângulo é uma entidade do nosso negócio
+public class Triangulo {
+    public double a;
+    public double b;
+    public double c;
+}
